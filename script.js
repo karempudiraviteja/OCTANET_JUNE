161b5745681cb1947522e3d4f620b69a11661faf
karempudiraviteja <146738829+karempudiraviteja@users.noreply.github.com>
2024-06-07 14:27:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,23 +6,4 @@ function closeNavbar() {
     document.getElementById("sideNavigationBar").style.width = "0";
 }
 
-document.getElementById('contactForm').addEventListener('Send Message', function(event) {
-    event.preventDefault();
-    
-    const form = this;
-    const email = form.email.value;
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (!emailPattern.test(email)) {
-        alert('Please enter a valid email address.');
-        return;
-    }
-
-    alert('Thank you for your message!');
-    
-
-    // Clear the form fields
-    form.reset();
-    
-        
-});
